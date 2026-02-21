f26788af3351e5f891d46c795bdbfddff5cc70db
Lenz Nicklas <lnicklas43@gmail.com>
2026-02-21 19:01:34 +0100

    Initial commit - Hyprland/Waybar/Fastfetch/Kitty/Neovim/rofi/

---
+++ .config/nvim/init.lua
@@ -0,0 +1,41 @@
+
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git","clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "auto",
+          section_separators = "",
+          component_separators = "",
+        },
+      })
+    end,
+  },
+})
+
+vim.cmd([[
+  hi Normal guibg=NONE ctermbg=NONE
+  hi NormalNC guibg=NONE ctermbg=NONE
+  hi EndOfBuffer guibg=NONE ctermbg=NONE
+]])
+
+vim.opt.laststatus = 0
+vim.opt.cmdheight = 0
+
+
