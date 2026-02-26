f5800ea8bec75278d56b91afdbe59603f1e045bc
Lenz Nicklas <lnicklas43@gmail.com>
2026-02-26 16:43:08 +0100

    Added Icon 'Playing Music' next to clock

---
+++ .config/waybar/scripts/music-icon.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+status=$(playerctl status 2>/dev/null)
+
+if [ "$status" = "Playing" ]; then
+    echo '{"text":"","class":"playing"}'
+elif [ "$status" = "Paused" ]; then
+    echo '{"text":"󰏤","class":"paused"}'
+else
+    echo '{"text":""}'
+fi
